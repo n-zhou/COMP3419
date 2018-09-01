fef582c8f7d0e71deabd9eb32de150567b9ecec7
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-09-02 02:43:35 +1000

    zzz

--- Lab Submission 1/src/main.py
+++ Lab Submission 1/src/main.py
@@ -4,6 +4,21 @@ import os
 import sys
 import math
 
+def step1():
+    pass
+
+def step2():
+    pass
+
+def step3():
+    pass
+
+def step4():
+    pass
+
+def step5():
+    pass
+
 def main(args):
     # K is preferred to be odd to make it easier to determine the central coordinate of
     # each grid block.
@@ -11,10 +26,11 @@ def main(args):
     if len(args) != 1:
         print("Error: Expected 1 argument but got %d" % len(args))
         sys.exit()
+
     PATH_TO_FILE = args[0]
-    vid = cv2.VideoCapture(PATH_TO_FILE)
     frames = []
     n_of_rows, n_of_columns, n_of_colour_channels = (None, None, None)
+    vid = cv2.VideoCapture(PATH_TO_FILE)
     while 1:
         ret, frame = vid.read()
         if not ret:
@@ -31,8 +47,6 @@ def main(args):
         cv2.imshow(PATH_TO_FILE, frame)
         if cv2.waitKey(15) & 0xFF == ord('q'):
             break
-    # SSD
-    # REPEAT 1-5???
     print(n_of_rows, n_of_columns, n_of_colour_channels)
 
 if __name__ == '__main__':
