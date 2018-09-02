8d42aed898e96309d64a78dd9ffd2837e6d8dedb
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-09-02 20:29:59 +1000

    more work

--- Lab Submission 1/src/main.py
+++ Lab Submission 1/src/main.py
@@ -6,8 +6,7 @@ import math
 
 # probably don't need this function
 def binarize(img):
-    img_bw = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]
-    return img_bw
+    return cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)[1]
 
 # probably don't need this function
 def invert(img):
@@ -19,10 +18,11 @@ def invert(img):
 
 # calculating the sum of squared differences
 def ssd(b1, b2):
+    # TODO replace with numpy functions
     sum = 0
     for x in range(b1.shape[0]):
         for y in range(b2.shape[1]):
-            for c in range(b2.shape[3])
+            for c in range(b2.shape[2]):
                 sum += math.pow(b1[x,y,c]-b2[x,y,c],2)
     return math.sqrt(sum)
 
@@ -53,7 +53,6 @@ def main(args):
     frames = []
     grey_frames = []
     binary_frames = []
-    inverted_frames = []
     n_of_rows, n_of_columns, n_of_colour_channels = (None, None, None)
     vid = cv2.VideoCapture(PATH_TO_FILE)
     # read in the frames
@@ -65,15 +64,19 @@ def main(args):
         frames.append(frame)
         grey_frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
         binary_frames.append(binarize(grey_frames[-1]))
-        inverted_frames.append(invert(binary_frames[-1]))
-        cv2.imshow('inverted', inverted_frames[-1])
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     # release the resources
     vid.release()
     cv2.destroyAllWindows()
 
-    for frame in inverted_frames:
+    yoyo = []
+    for i range(len(frames)):
+        yoyo.append({})
+        for x in range(int(n_of_columns/K))
+            for y in range(int(n_of_rows/K)):
+                yoyo[i][(x,y)] = np.zeros(K,K)
+    for frame in binary_frames:
         cv2.imshow(PATH_TO_FILE, frame)
         if cv2.waitKey(15) & 0xFF == ord('q'):
             break
