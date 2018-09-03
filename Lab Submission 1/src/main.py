ddb52fc5790209eb2e1b18d204c0298932852be1
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-09-04 01:23:50 +1000

    clean

--- Lab Submission 1/src/main.py
+++ Lab Submission 1/src/main.py
@@ -9,6 +9,10 @@ import math
 def ssd(b1, b2):
     return math.sqrt(np.sum((b1-b2)**2))
 
+# check that the boundary we are checking is valid
+def check_valid(img, x1, x2, y1, y2):
+    return x1 >= 0 and x2 <= img.shape[0] and y1 >= 0 and y2 <= img.shape[1]
+
 def step1(img, k = 8):
     pass
 
@@ -31,19 +35,17 @@ if __name__ == '__main__':
     K = 8
     PATH_TO_FILE = 'monkey.avi'
     frames = []
-    n_of_rows, n_of_columns, n_of_colour_channels = (None, None, None)
     cap = cv2.VideoCapture(PATH_TO_FILE)
     # read in the frames
     while 1 and len(frames) < 250:
         ret, frame = cap.read()
         if not ret:
             break
-        n_of_rows, n_of_columns, n_of_colour_channels = frame.shape
         frames.append(frame)
     # release the resources
     cap.release()
     cv2.destroyAllWindows()
-
+z
     for frame in frames:
         cv2.imshow(PATH_TO_FILE, frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
