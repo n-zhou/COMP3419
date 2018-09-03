485252c1c16661e0139bd7f180d39a6392b120ad
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-09-03 23:52:39 +1000

    making sure things are up to date

--- Lab Submission 1/src/main.py
+++ Lab Submission 1/src/main.py
@@ -38,44 +38,29 @@ def step4():
 def step5():
     pass
 
-def main(args):
+
+if __name__ == '__main__':
     # K is preferred to be odd to make it easier to determine the central coordinate of
     # each grid block.
     K = 8
-    if len(args) != 1:
-        print("Error: Expected 1 argument but got %d" % len(args))
-        sys.exit()
-
-    PATH_TO_FILE = args[0]
+    PATH_TO_FILE = 'monkey.avi'
     frames = []
     grey_frames = []
     binary_frames = []
     n_of_rows, n_of_columns, n_of_colour_channels = (None, None, None)
     cap = cv2.VideoCapture(PATH_TO_FILE)
     # read in the frames
-    while 1:
+    while 1 and len(frames) < 250:
         ret, frame = cap.read()
         if not ret:
             break
         n_of_rows, n_of_columns, n_of_colour_channels = frame.shape
         frames.append(frame)
-        grey_frames.append(cv2.cvtColor(frames[-1], cv2.COLOR_BGR2GRAY))
-        binary_frames.append(binarize(grey_frames[-1]))
     # release the resources
     cap.release()
     cv2.destroyAllWindows()
-
-    splits = []
-    for i in range(len(frames)):
-        splits.append({})
-        for x in range(int(n_of_columns/K)):
-            for y in range(int(n_of_rows/K)):
-                splits[-1][(x,y)] = np.zeros((K,K))
 
     for frame in frames:
         cv2.imshow(PATH_TO_FILE, frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
