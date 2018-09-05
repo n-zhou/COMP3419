3f906a25e7068e7d271b8df88aa2c3e8335ced23
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-09-05 15:21:29 +1000

    made some more code

--- Lab Submission 1/src/main.py
+++ Lab Submission 1/src/main.py
@@ -15,11 +15,13 @@ def check_valid(img, x1, x2, y1, y2):
 
 def magic(frame1, frame2, k = 8):
     # they come in size 2 because we hold 2 vectors
-    displacement_vectors = np.array((int(frame1.shape[0]/k), int(frame1.shape[1]/k), 2))
+    displacement_vectors = np.ones((int(frame1.shape[0]/k), int(frame1.shape[1]/k), 2))
     for x in range(0,frame1.shape[0],k):
         for y in range(0,frame1.shape[1],k):
             if check_valid(frame1,x,x+k,y,y+k):
                 macroblock = frame1[x:x+k,y:y+k]
+
+
     # return the displacement vectors xd
     return displacement_vectors
 
@@ -41,8 +43,18 @@ if __name__ == '__main__':
     cap.release()
     cv2.destroyAllWindows()
 
-    counter = 0
-    for frame in frames:
-        step1(frame)
-        cv2.imwrite('./output/frame%d.png' % counter, frame)
-        counter += 1
+    # write an output vid
+    out = cv2.VideoWriter('outputVid.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 24, (int(frames[-1].shape[1]), int(frames[-1].shape[0])))
+    for index in range(1,len(frames)):
+        copy = np.copy(frames[index])
+        for x in range(int(copy.shape[0]/K)):
+            for y in range(int(copy.shape[1]/K)):
+                displacement_vectors = magic(frames[index-1], frames[index],K)
+                if displacement_vectors[x,y,0] == 0 and displacement_vectors[x,y,1] == 0:
+                    continue
+                copy = cv2.circle(copy, (int(y*K + displacement_vectors[x,y,1]), int(x*K + displacement_vectors[x,y,0])), 1, (0,0,255), thickness=1, lineType=-1, shift=0)
+        cv2.imwrite('./output/frame%d.png' % index, copy)
+        out.write(copy)
+
+    out.release()
+    cv2.destroyAllWindows()
