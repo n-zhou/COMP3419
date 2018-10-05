86aecfe58d2f748b83e94f355dbf639299d53334
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-10-05 16:55:26 +1000

    segmented the red markers

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,34 +1,32 @@
 import cv2
 import numpy as np
 
-'''
-lower_blue = np.array([110,50,50])
-upper_blue = np.array([130,255,255])
-'''
+class IntelligentObject():
 
-lower_blue = np.array([0,0,0])
-upper_blue = np.array([180,255,255])
+    def __init__(self):
+        pass
+        
+lower_hsv = np.array([160,100,100])
+upper_hsv = np.array([179,255,255])
 
-def replace(background, foreground):
-    ret = np.copy(backgound)
+def replace(foreground):
+    ret = np.copy(foreground)
     hsv = cv2.cvtColor(foreground, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
     res = cv2.bitwise_and(foreground,foreground, mask= mask)
-
+    return res
     outimageHSV = np.uint8(res)
     outimageBGR = cv2.cvtColor(outimageHSV, cv2.COLOR_HSV2BGR)
-
+    '''
     for x in range(ret.shape[0]):
         for y in range(ret.shape[1]):
             if not np.sum(outimageBGR[x,y]):
                 ret[x,y] = foreground[x,y]
     return ret
+    '''
 
 
 if __name__ == '__main__':
-    # read in the background
-    backgound = cv2.imread('background.jpg')
-
     # read in the video
     cap = cv2.VideoCapture('monkey (option1).mov')
 
@@ -47,7 +45,7 @@ if __name__ == '__main__':
 
 
     for img in frames:
-        cv2.imshow('show', replace(backgound, img))
-        if cv2.waitKey(1) == ord('q'):
+        cv2.imshow('show', replace(img))
+        if cv2.waitKey(15) == ord('q'):
             break
     cv2.destroyAllWindows()
