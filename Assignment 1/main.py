438370db9a94cd951280e6ac64d00ebfd4361758
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-10-06 01:30:07 +1000

    added some more code back in

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -7,7 +7,13 @@ class IntelligentObject():
         pass
 
 def threshold_red(img):
-    return img
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    frame_threshold1 = cv2.inRange(hsv_img, np.array([0, 100, 100],np.uint8), np.array([10, 255, 255],np.uint8))
+    frame_threshold2 = cv2.inRange(hsv_img, np.array([160, 100, 100],np.uint8), np.array([179, 255, 255],np.uint8))
+    final_threshold = frame_threshold1 + frame_threshold2
+    erosion = cv2.erode(final_threshold,np.ones((3,3)),iterations = 4)
+    dilation = cv2.dilate(erosion,np.ones((3,3)),iterations = 4)
+    return erosion
 
 
 if __name__ == '__main__':
