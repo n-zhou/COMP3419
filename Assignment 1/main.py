8fe35ad3922be7c2bb3c6eec151a69b981bedf30
nzho8446 <nzho8446@uni.sydney.edu.au>
2018-10-07 01:14:26 +1000

    added trumps face

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -10,7 +10,7 @@ def play_random_sound():
     from threading import Thread
     def play(arg):
         import winsound
-        winsound.PlaySound('./sounds/fired.wav', winsound.SND_FILENAME)
+        winsound.PlaySound('./sounds/fired2.wav', winsound.SND_FILENAME)
     Thread(target=play, args=(None,)).start()
 
 def threshold_red(img):
@@ -18,20 +18,20 @@ def threshold_red(img):
     frame_threshold1 = cv2.inRange(hsv_img, np.array([0, 100, 100],np.uint8), np.array([10, 255, 255],np.uint8))
     frame_threshold2 = cv2.inRange(hsv_img, np.array([160, 100, 100],np.uint8), np.array([179, 255, 255],np.uint8))
     final_threshold = frame_threshold1 + frame_threshold2
-    erosion = cv2.erode(final_threshold,np.ones((3,3)),iterations=2)
-    dilation = cv2.dilate(erosion,np.ones((3,3)),iterations=2)
-    return cv2.adaptiveThreshold(dilation,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-        cv2.THRESH_BINARY,11,2)
+    kernel = np.ones((3,3))
+    opening = cv2.morphologyEx(final_threshold, cv2.MORPH_OPEN, kernel)
+    return opening
+
 
 def combine(bg, fg):
     ret = np.copy(bg)
-    thesholded_img = threshold_red(fg)
+    t_channel_fg = cv2.cvtColor(fg, cv2.COLOR_BGRA2BGR)
+    #thesholded_img = threshold_red(fg)
     for x in range(bg.shape[0]):
         for y in range(bg.shape[1]):
-            if thesholded_img[x,y] != 0:
-                ret[x,y,0] = fg[x,y,0]
-                ret[x,y,1] = fg[x,y,1]
-                ret[x,y,2] = fg[x,y,2]
+            if x < fg.shape[0] and y < fg.shape[1]:
+                if fg[x,y,0] != 0:
+                    ret[x,y] = t_channel_fg[x,y]
     return ret
 
 if __name__ == '__main__':
@@ -51,14 +51,12 @@ if __name__ == '__main__':
         frames.append(frame)
     # cleanup
     cap.release()
-
-
-    play_random_sound()
     #out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 24, (int(width), int(height)))
     for img in frames:
         #out.write(combine(background, img))
-        cv2.imshow('show', combine(background, img))
-        if cv2.waitKey(15) == ord('q'):
+        trump = cv2.imread('trump.png', cv2.IMREAD_UNCHANGED)
+        cv2.imshow('show', combine(background,trump))
+        if cv2.waitKey(30) == ord('q'):
             break
     #out.release()
     cv2.destroyAllWindows()
